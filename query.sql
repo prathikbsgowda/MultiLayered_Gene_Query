4a3bad0e07d09724540232184452b6b9330be93e
Prathik B S Gowda <96683588+prathikbsgowda@users.noreply.github.com>
2024-09-18 16:08:49 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -4,24 +4,26 @@ WITH kin AS (SELECT DISTINCT  co_bi_inter_groupidconditions.condition_id_id,site
 						 ON co_bi_inter_conditiongroups.condition_group_id=co_bi_inter_groupidconditions.groupid_id 
 						WHERE co_bi_inter_conditiongroups.gene_id = 'PAK1'
 				),
+	
 inter_binary AS (
     SELECT DISTINCT a.GENE_ID
     FROM co_bi_inter_genebinayinter a
     JOIN co_bi_inter_genebinayinter b ON a.interactor_id = b.interactor_id 
     WHERE b.gene_id='PAK1'
-						),						
+						),
+	
 inter AS (
 			SELECT DISTINCT  co_bi_inter_groupidconditions.condition_id_id,site_id,gene_id FROM co_bi_inter_conditiongroups 
  			JOIN co_bi_inter_groupidconditions ON co_bi_inter_conditiongroups.condition_group_id=co_bi_inter_groupidconditions.groupid_id 
 			WHERE co_bi_inter_conditiongroups.gene_id IN (SELECT * FROM inter_binary)
 			),
 	
-
 count_kin AS (
 					SELECT site_id,COUNT(DISTINCT co_bi_inter_groupidconditions.condition_id_id) AS kin_freq,gene_id AS kin 
 					FROM co_bi_inter_conditiongroups JOIN co_bi_inter_groupidconditions
 					ON co_bi_inter_conditiongroups.condition_group_id=co_bi_inter_groupidconditions.groupid_id 
 					WHERE co_bi_inter_conditiongroups.gene_id = 'PAK1' GROUP BY site_id),
+	
 	
 count_inter AS (
 					SELECT   count(DISTINCT co_bi_inter_groupidconditions.condition_id_id)AS interact_freq,site_id,gene_id 
@@ -40,7 +42,6 @@ shared_freq AS (
 SELECT shared_freq.kin,kin_sit,kin_freq,shared_freqn,Binary_interactor,shared_freq.site_id,interact_freq,
 			LEAST ( kin_freq , interact_freq )AS min_freq ,
 			(CAST(shared_freqn AS DECIMAL(10, 2)) / CAST(LEAST( kin_freq , interact_freq ) AS DECIMAL(10, 2))) AS shared_score
-	
 				FROM shared_freq JOIN count_kin ON count_kin.site_id = shared_freq.kin_sit 
 				JOIN count_inter ON shared_freq.Binary_interactor = count_inter.gene_id 
 				AND shared_freq.site_id = count_inter.site_id
