85d01980ac0519dfa9dfae58abbb4243757e0a7d
Prathik B S Gowda <96683588+prathikbsgowda@users.noreply.github.com>
2025-01-03 09:42:34 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -2,7 +2,7 @@
 WITH kin AS (SELECT DISTINCT  co_bi_inter_groupidconditions.condition_id_id,site_id AS kin_sit,gene_id AS kin 
 						FROM co_bi_inter_conditiongroups JOIN co_bi_inter_groupidconditions
 						 ON co_bi_inter_conditiongroups.condition_group_id=co_bi_inter_groupidconditions.groupid_id 
-						WHERE co_bi_inter_conditiongroups.gene_id = 'TTN'
+						WHERE co_bi_inter_conditiongroups.gene_id = 'PAK1'
 	),
 	
 inter_binary AS (
